40db45b37f5b3ae2c07acaf2555c0cdff3b76e24
Ryan Gerry <gerryster@gmail.com>
2013-07-19 15:55:53 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4V3Sourcemaps::Application.config.secret_key_base = '46fa7c8f679c2ad71d3ab357c83cf530263ae84c7536dfab853b73999929991b24db1bd4596974d5ea93d891daa1ee8f056afdd30c1ddb65162e807a94395cd2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails4V3Sourcemaps::Application.config.session_store :cookie_store, key: '_rails4-v3-sourcemaps_session'
