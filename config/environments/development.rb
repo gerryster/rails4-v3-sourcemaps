e7975cd4b1dc3d186a6e591702a02bf5bf988917
Ryan Gerry <gerryster@gmail.com>
2013-07-20 07:42:38 -0500

    Source maps are working in a static application in Chrome Canary.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Rails4V3Sourcemaps::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # SASS
+  #config.sass.sourcemap = true
 end
